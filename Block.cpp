276df57531a18755e34d8ef4dc8157eef8972364
tdouglasantony <tdouglasantony@gmail.com>
2019-12-13 17:15:43 -0600

    Documentation

--- Block.cpp
+++ Block.cpp
@@ -21,6 +21,8 @@ Block::Block(int blockNumber, int nextBlock, vector<Record> records)
 
 /** Returns a vector of strings containing the block number, next block, size, and records (in order)
 of the block object.
+@pre: A block object
+@post: a vector of string containing information about the records of the block object
 */
 vector<string> Block::pack() {
 	 vector<string> packed = vector<string>();
@@ -50,6 +52,8 @@ int Block::getBlockNumber()
 
 /** Returns the record located at a given index.
 @param index: the index within the block where the record is located
+@pre: an integer index
+@post: the record within the block at the index
 */
 Record Block::getRecord(int index) {
 	 return records[index];
@@ -70,6 +74,11 @@ string Block::getLastKey()
 }
 
 
+/** Erases the record at location rrn
+@param rrn: The relative record number of the record to be deleted from the block
+@pre: a relative record number within the bounds of the block record size
+@post: a block with the record previously located at the rrn, now without that record
+*/
 void Block::deleteRecord(int rrn) {
 	 records.erase(records.begin() + rrn);
 }
@@ -95,6 +104,8 @@ Record Block::pop_last()
 /** Inserts a record at a given index
 @param index: position at which the record is to be inserted.
 @param rec: the record which is to be inserted into the block.
+@pre: an index between zero and the block number, a record
+@post: the block has the record inserted at the index input
 */
 void Block::insertRecord(int index, Record rec)
 {
@@ -103,6 +114,8 @@ void Block::insertRecord(int index, Record rec)
 
 /**Appends a record onto the end of the block's record vector
 @param rec: the record which is to be appended to the block.
+@pre: a record
+@post: the record is inserted at the end of the record vector in the block
 */
 void Block::pushRecord(Record rec)
 {
@@ -112,6 +125,8 @@ void Block::pushRecord(Record rec)
 /** Takes a block in the form of a vector of strings and converts it to a block object
 @param packedBlock: the vector of strings with the block information to be converted
 into a block object.
+@pre: a vector of strings containing the block information
+@post: a block with information that was stored in the packedBlock vector
 */
 void Block::unpack(vector<string> packedBlock)
 {
